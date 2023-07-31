2a0d354f0f08d280feedea5ae1c7ed5f3411347d
PWall <34860495+PWall2222@users.noreply.github.com>
2023-07-31 11:17:03 +0200

    feat: enable all loggers via env

--- ValLib/debug.py
+++ ValLib/debug.py
@@ -14,6 +14,22 @@ class Level(IntEnum):
     VERBOSE = 1
 
 
+class LogEnv:
+    level: int
+    console: bool
+    file: bool
+    path: Path
+
+    @classmethod
+    def env(cls, logger: logging.Logger):
+        logenv = cls()
+        logenv.level = int_env(logger, "LOG_LEVEL", Level.INFO)
+        logenv.console = bool_env(logger, "LOG_CONSOLE", True)
+        logenv.file = bool_env(logger, "LOG_FILE_ENABLE", False)
+        logenv.path = Path(get_env(logger, "LOG_FILE", ""))
+        return logenv
+
+
 def get_env(logger: logging.Logger, key: str, default):
     return getenv(f"{logger.name}_{key}", default)
 
@@ -46,16 +62,25 @@ def file_out(logger: logging.Logger, path: Path):
     logger.addHandler(h)
 
 
+def general():
+    l = LogEnv()
+    l.level = int(getenv("VALUTILS_LEVEL", Level.INFO))
+    l.console = bool(int(getenv("VALUTILS_CONSOLE", False)))
+    l.file = bool(int(getenv("VALUTILS_FILE_ENABLE", False)))
+    l.path = Path(getenv("VALUTILS_FILE", ""))
+    return l
+
+
 def env_setup(logger: logging.Logger):
-    level = int_env(logger, "LOG_LEVEL", Level.INFO)
-    logger.setLevel(level)
-    console = bool_env(logger, "LOG_CONSOLE", True)
-    if console:
+    l = LogEnv.env(logger)
+    gen = general()
+    logger.setLevel(min(l.level, gen.level))
+    if l.console or gen.console:
         stderr(logger)
-    file = bool_env(logger, "LOG_FILE_ENABLE", False)
-    file_path = Path(get_env(logger, "LOG_FILE", ""))
-    if file:
-        file_out(logger, file_path)
+    if l.file:
+        file_out(logger, l.path)
+    if gen.file:
+        file_out(logger, gen.path)
 
 
 add_levels()
