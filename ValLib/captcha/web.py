37c4fbcf6ebc96b98cce979391de4128917f1254
PWall <34860495+PWall2222@users.noreply.github.com>
2023-09-26 15:01:06 +0200

    fix: set port inside a thread for non blocking

--- ValLib/captcha/web.py
+++ ValLib/captcha/web.py
@@ -1,5 +1,5 @@
 from http.server import ThreadingHTTPServer as HTTPServer
-from threading import Timer
+from threading import Thread, Timer
 from webbrowser import open as open_url
 
 from .exceptions import HCaptchaTimeoutException
@@ -11,16 +11,26 @@ from .struct import CaptchaSolver
 class WebServerSolver(CaptchaSolver):
     _server: HTTPServer
     _address: str
-    _port: int
+    _port: int = 8080
     _timeout: int
     result: str
 
+    def _init_port(self):
+        def set_port():
+            self._port = next(random_port())
+            self._init_server()
+        t = Thread(target=set_port)
+        t.start()
+
+    def _init_server(self):
+        if hasattr(self, "_server"):
+            return
+        self._server = HTTPServer((self._address, self._port), CaptchaHandler)
+
     def __init__(self, address="localhost", timeout=30):
-        port = next(random_port())
         self._address = address
-        self._port = port
         self._timeout = timeout
-        self._server = HTTPServer((address, port), CaptchaHandler)
+        self._init_port()
 
     def _finish(self):
         result = self.result
@@ -39,6 +49,7 @@ class WebServerSolver(CaptchaSolver):
         return t
 
     def token(self, rqdata: str, site_key: str):
+        self._init_server()
         CaptchaHandler.rqdata = rqdata
         CaptchaHandler.site_key = site_key
         CaptchaHandler.parent = self
