0c95719499ce41832b65d97e78110a9289c49f91
PWall <34860495+PWall2222@users.noreply.github.com>
2023-10-30 07:00:25 +0100

    chore: add AuthException as export

--- ValLib/__init__.py
+++ ValLib/__init__.py
@@ -1,8 +1,10 @@
 from .auth import *
+from .exceptions import *
 from .structs import *
 
 __all__ = [
     "authenticate", "cookie_token",
     "User", "Auth", "Token",
     "make_headers",
+    "AuthException",
 ]
